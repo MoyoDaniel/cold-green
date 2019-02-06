764f744e40330421e01cd90225df0c7e09f08d87
MoyoDaniel <40076524+MoyoDaniel@users.noreply.github.com>
2019-02-06 04:32:06 +0100

    Add files via upload

---
+++ MoyoQuizz.js
@@ -0,0 +1,14 @@
+﻿var person = {
+name: ['Ogunyemi', 'Moyosoluwa'],
+age: 25,
+gender: Male,
+interests: [' I love to program and also learn more about Web design', 'I also love to participate in track event'],
+bio.function() {
+alert(this.name[0] + ' ' + this.name[1] + ' is' + this.age +
+'years old. she likes ' + this.interests[0] + 'and' +
+this.interests[1] + '.');
+},
+greeting: function() {
+alert('Hi! I\ 'm ' + thiss.name[0] + '.');
+}
+};
